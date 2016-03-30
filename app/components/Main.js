cf16ab2260cf559bbab3e26c5ed58a06544a9090
Kamal Hothi <kamalren@gmail.com>
2016-03-30 21:37:56 +1300

    Adding App.js as entry point for webpack.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,6 +8,7 @@ var Main = React.createClass({
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm-offset-2" style={{marginTop:15}}>
             Menu
+          </div>
           </nav>
           <div className="container">
             {this.props.children}
