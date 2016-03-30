f5cb79c9ede6f5e0757bb3f4bf719b62e7fc31d3
Kamal Hothi <kamalren@gmail.com>
2016-03-30 17:33:11 +1300

    Changing Main component

--- app/components/Main.js
+++ app/components/Main.js
@@ -4,9 +4,15 @@ var ReactDOM = require('react-dom');
 var Main = React.createClass({
   render: function(){
     return (
-      <div>
-        Hello World
-      </div>
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm-7 col-sm-offset-2" style={{marginTop:15}}>
+            Menu
+          </nav>
+          <div className="container">
+            {this.props.children}
+          </div>
+        </div>
     )
   }
 });
